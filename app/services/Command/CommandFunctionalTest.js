22d565d8be6d5900e216af5247cebefad503e97b
Stanislav Stepanenko <dsazztazz@gmail.com>
2017-03-16 13:16:34 +0200

    Fixed bug with path generate

--- app/services/Command/CommandFunctionalTest.js
+++ app/services/Command/CommandFunctionalTest.js
@@ -6,15 +6,13 @@
  * Licensed under MIT (https://github.com/linkshare/plus.garden/blob/master/LICENSE)
  * ============================================================================== */
 
- var CommandFunctionalTest = function (command, config, commander, logger) {
+var CommandFunctionalTest = function (command, config, commander, logger) {
 
     var wait = require('wait.for');
-    var fs = require('fs');
     var path = require('path');
     var execSync = require('child_process').execSync;
 
     this.run = function (name) {
-
         var name = name || 'features';
         var npmBin = execSync('npm bin').toString().replace(/(\r\n|\n|\r)/gm, '');
         var cmd = path.join(npmBin, 'cucumber-js');
@@ -24,13 +22,13 @@
         }
 
         if (name) {
-            var requireDir  = name.replace('\\', '/').split('/');
-            requireDir = commander.require || requireDir;
+            var requireDir = commander.require || name;
 
-            cmd += ' --require ' + requireDir;
+            cmd += ' ' + requireDir;
         }
 
         cmd += ' --format pretty ' + name;
+        console.log(cmd);
 
         wait.forMethod(command, 'run', cmd);
     };
